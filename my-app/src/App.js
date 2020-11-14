8e297b9add4e968f6665c504991d622332bfa755
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-13 23:27:18 -0800

    create useEffect function for getRecipes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
@@ -7,10 +6,25 @@ const App = () => {
   const APP_ID = '3f56839b';
   const APP_KEY = '680f32662e776eca58f997ba9fab02ec';
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    console.log(data.hits);
+  }
+
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   )
 }
